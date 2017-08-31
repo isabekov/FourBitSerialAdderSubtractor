bbf2b14c91e2cfc210453d1516a6b881b271fa3a
Altynbek Isabekov <aisabekov@ku.edu.tr>
2017-08-31 21:36:32 +0300

    Possible extension to N bits is added.

--- VHDL/FourBitSerialAdderSubtractor.vhd
+++ VHDL/FourBitSerialAdderSubtractor.vhd
@@ -2,22 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity FourBitSerialAdderSubtractor is
-port (
-    A: in STD_LOGIC_VECTOR (3 downto 0); -- Input number A
-    B: in STD_LOGIC_VECTOR (3 downto 0); -- Input number B
+generic (N : INTEGER:=4); -- N-bit Serial Adder/Subtractor
+port(
+    A: in STD_LOGIC_VECTOR (N-1 downto 0); -- Input number A
+    B: in STD_LOGIC_VECTOR (N-1 downto 0); -- Input number B
     CLK: in STD_LOGIC; -- Clock signal
     Mode: in STD_LOGIC; -- Operating mode (addition/subtraction)
     Load: in STD_LOGIC; -- Parallel load
-    A_Reg_Out: out STD_LOGIC_VECTOR(3 downto 0); -- Output of register A
-    B_Reg_Out: out STD_LOGIC_VECTOR(3 downto 0); -- Output of register B
+    A_Reg_Out: out STD_LOGIC_VECTOR(N-1 downto 0); -- Output of register A
+    B_Reg_Out: out STD_LOGIC_VECTOR(N-1 downto 0); -- Output of register B
     Sev_Seg_Out: out STD_LOGIC_VECTOR(6 downto 0)); -- Seven segment output
 end FourBitSerialAdderSubtractor;
 
 architecture Behavioral of FourBitSerialAdderSubtractor is
-
 signal CarryOut: STD_LOGIC; -- Carry-out signal
-signal A_Reg: STD_LOGIC_VECTOR(3 downto 0):="0000"; -- Register A
-signal B_Reg: STD_LOGIC_VECTOR(3 downto 0):="0000"; -- Register B
+signal A_Reg: STD_LOGIC_VECTOR(N-1 downto 0):=(others => '0'); -- Register A
+signal B_Reg: STD_LOGIC_VECTOR(N-1 downto 0):=(others => '0'); -- Register B
 
 begin
 -------------- Sequential part --------------
@@ -39,11 +39,11 @@ if(rising_edge(CLK)) then
         end if;
     else
         -- Rotate right register B_Reg
-        B_Reg <= (B_Reg(0) & B_Reg(3 downto 1));
+        B_Reg <= (B_Reg(0) & B_Reg(N-1 downto 1));
         -- Full-adder: summation output is assigned to variable "Sum" (immediate)
         Sum := (A_Reg(0) xor B_Reg(0)) xor (CarryOut);
         -- Write sum output of the full-adder to the MSB of the register A_Reg
-        A_Reg <= (Sum & A_Reg(3 downto 1));
+        A_Reg <= (Sum & A_Reg(N-1 downto 1));
         -- Write carry out output of the full-adder to the flip-flop
         CarryOut <= (((A_Reg(0) xor B_Reg(0)) and CarryOut)) or (A_Reg(0) and B_Reg(0));
     end if;
@@ -55,22 +55,21 @@ A_Reg_Out <= A_Reg; -- Map A_REG to LEDs
 B_Reg_Out <= B_Reg; -- Map B_REG to LEDs
 
 -- Seven segment display for register A_Reg
-with A_Reg select
-Sev_Seg_Out <=  "0000001" when "0000",
-                "1001111" when "0001",
-                "0010010" when "0010",
-                "0000110" when "0011",
-                "1001100" when "0100",
-                "0100100" when "0101",
-                "0100000" when "0110",
-                "0001111" when "0111",
-                "0000000" when "1000",
-                "0000100" when "1001",
-                "0001000" when "1010",
-                "1100000" when "1011",
-                "0110001" when "1100",
-                "1000010" when "1101",
-                "0110000" when "1110",
-                "0111000" when "1111",
-                "0000001" when others;
+Sev_Seg_Out <=  "0000001" when A_Reg = "0000" else
+                "1001111" when A_Reg = "0001" else
+                "0010010" when A_Reg = "0010" else
+                "0000110" when A_Reg = "0011" else
+                "1001100" when A_Reg = "0100" else
+                "0100100" when A_Reg = "0101" else
+                "0100000" when A_Reg = "0110" else
+                "0001111" when A_Reg = "0111" else
+                "0000000" when A_Reg = "1000" else
+                "0000100" when A_Reg = "1001" else
+                "0001000" when A_Reg = "1010" else
+                "1100000" when A_Reg = "1011" else
+                "0110001" when A_Reg = "1100" else
+                "1000010" when A_Reg = "1101" else
+                "0110000" when A_Reg = "1110" else
+                "0111000" when A_Reg = "1111" else
+                "0000001";
 end Behavioral;
